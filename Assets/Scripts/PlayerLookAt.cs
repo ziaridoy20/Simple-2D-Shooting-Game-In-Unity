75073eca8115789c859e593f3185f2764ad314cb
ziaridoy20 <ziaridoy20@gmail.com>
2015-03-20 21:17:27 +0600

    First Cimmit
    First Commit 20 March 15

---
+++ Assets/Scripts/PlayerLookAt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLookAt : MonoBehaviour {
+	
+	private Vector3 mousePosition;
+	private float rotationAngle;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		Vector3 dir = mousePosition - transform.position;
+		float angle = (((Mathf.Atan2(dir.y, dir.x)) * Mathf.Rad2Deg)-90);
+		transform.rotation = Quaternion.AngleAxis(angle, transform.forward);
+		
+	}
+	
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	
+	public float moveSpeed = 5;
+	private Vector3 moveVector;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		moveVector = new Vector3(Input.GetAxisRaw ("Horizontal"),Input.GetAxisRaw("Vertical"),0);
+		moveVector.Normalize();
+		
+		transform.Translate(moveVector*Time.deltaTime*moveSpeed,Space.World);
+		
+	}
+	
+}
